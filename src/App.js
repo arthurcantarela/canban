25e7a122ba7c257d97e9df33168892d3987ac6e3
Arthur Cantarela <cantarela.arthur@gmail.com>
2018-10-20 15:13:54 -0300

    Adding some mock data and basic components for kanban

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const Card = ({id, title, text}) => (
+  <li>
+    <h3>{title}</h3>
+    <p>{text}</p>
+  </li>
+)
+
+const Column = ({id, title, cards}) => (
+  <ul>
+    <h2>{title}</h2>
+    {cards.map(({id, ...card}) => (
+      <Card key={id} id={id} {...card} />
+    ))}
+  </ul>
+);
+
+const App = ({columns}) => (
+  <main>
+    {columns.map(({id, ...column}) => (
+      <Column key={id} id={id} {...column} />
+    ))}
+  </main>
+);
+
+const hardcodedData = {
+  columns: [
+    {
+      id: 1,
+      title: "To Do",
+      cards: [
+        {
+          id: 1,
+          title: "Task One",
+          text: "Lorem ipsum dolor sit amet"
+        },
+        {
+          id: 2,
+          title: "Task Two",
+          text: "Lorem ipsum dolor sit amet"
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "Done",
+      cards: [
+        {
+          id: 3,
+          title: "Task One",
+          text: "Lorem ipsum dolor sit amet"
+        },
+        {
+          id: 4,
+          title: "Task Two",
+          text: "Lorem ipsum dolor sit amet"
+        }
+      ]
+    }
+  ]
 }
+
+App.defaultProps = {
+  ...hardcodedData
+};
 
 export default App;
