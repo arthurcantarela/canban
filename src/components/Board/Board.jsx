355d40e57119d1cc9068de8a5e3d0a16b47f069e
Arthur Cantarela <cantarela.arthur@gmail.com>
2018-10-22 14:54:34 -0300

    Starting implementing drag n drop

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { DragDropContext } from "react-beautiful-dnd";
 import { Column } from ".";
 import Button from "components/Button";
 import "./Board.scss";
 
 const Board = ({ columns, addColumn, addCard }) => (
-  <section className="Board">
-    {columns.map(({ id, ...column }) => (
-      <Column key={id} id={id} {...column} addCard={addCard} />
-    ))}
-    <Button onClick={() => addColumn()}>Add column</Button>
-  </section>
+  <DragDropContext onDragEnd={result => {}}>
+    <section className="Board">
+      {columns.map(({ id, ...column }) => (
+        <Column key={id} id={id} {...column} addCard={addCard} />
+      ))}
+      <Button onClick={() => addColumn()}>Add column</Button>
+    </section>
+  </DragDropContext>
 );
 
 export default Board;
--- src/components/Board/Column.jsx
+++ src/components/Board/Column.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { Droppable } from "react-beautiful-dnd";
 import { Card } from ".";
 import Button from "components/Button";
 import "./Column.scss";
 
 const Column = ({ id, title, cards, addCard }) => (
-	<ul className="Column">
-		<h2 className="ColumnTitle">{title}</h2>
-		{cards.map(({ id, ...card }) => (
-			<Card key={id} id={id} {...card} />
-		))}
-		<Button onClick={() => addCard(id)}>
-			Add card
-		</Button>
-	</ul>
+  <Droppable droppableId={id}>
+    {provided => (
+      <ul
+        className="Column"
+        innerRef={provided.innerRef}
+        {...provided.droppableProps}
+      >
+        <h2 className="ColumnTitle">{title}</h2>
+        {cards.map(({ id, ...card }) => (
+          <Card key={id} id={id} {...card} />
+        ))}
+        <Button onClick={() => addCard(id)}>Add card</Button>
+        {provided.placeholder}
+      </ul>
+    )}
+  </Droppable>
 );
 
 export default Column;
